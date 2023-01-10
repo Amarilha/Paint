f5cceb571df25d7d4ed6349b81e21655a4adaf01
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2023-01-10 22:08:02 +0100

    Update Paint.py

--- Paint.py
+++ Paint.py
@@ -9,6 +9,23 @@ root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
+current_x=0
+current_y=0
+color = 'black'
+
+def locate_xy(work):
+    
+    global current_x, current_y
+    
+    current_x= work.x
+    current_y= work.y
+    
+def addLine(work):
+    global current_x,current_y
+    
+    canvas.create_line((current_x,current_y,work.x,work.y),width=2, fill=color)
+    current_x,current_y = work.x, work.y
+
 #icon
 image_icon= PhotoImage(file="image/pincel.png")
 root.iconphoto(False,image_icon)
@@ -59,8 +76,8 @@ canvas =Canvas(root,width=930,height=500,background="white",cursor="hand2")
 canvas.place(x=100, y=10)
 
 
-canvas.bind('<Button-1>')
-canvas.bind('<B1-Motion>')
+canvas.bind('<Button-1>', locate_xy)
+canvas.bind('<B1-Motion>', addLine)
 
 
 root.mainloop()
