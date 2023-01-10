48c5005f741713371fd0cc2c13c6489be755e307
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2023-01-10 22:36:43 +0100

    final

--- Paint.py
+++ Paint.py
@@ -23,8 +23,16 @@ def locate_xy(work):
 def addLine(work):
     global current_x,current_y
     
-    canvas.create_line((current_x,current_y,work.x,work.y),width=2, fill=color)
+    canvas.create_line((current_x,current_y,work.x,work.y),width= get_current_value(), fill=color, capstyle=ROUND, smooth=TRUE )
     current_x,current_y = work.x, work.y
+    
+def show_color(new_color):
+    global color
+    color = new_color
+
+def new_canvas():
+    canvas.delete('all')
+    display_pallete()
 
 #icon
 image_icon= PhotoImage(file="image/pincel.png")
@@ -34,38 +42,38 @@ color_box=PhotoImage(file="image/menos (3).png" )
 Label(root,image=color_box,bg="#f2f3f5").place(x=10, y=20)
 
 eraser=PhotoImage(file="image/eraser (1).png")
-Button(root, image=eraser,bg="#f2f3f5").place(x=25.5, y=400)
+Button(root, image=eraser,bg="#f2f3f5",command=new_canvas).place(x=25.5, y=400)
 
 colors=Canvas(root,bg="#ffffff",width=32,height=300,bd=0)
 colors.place(x=20, y=60)
 
 def display_pallete():
     id = colors.create_rectangle((3.5,10,30,30),fill='black')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('black'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('black'))
     
     id = colors.create_rectangle((3.5,40,30,60),fill='gray')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('gray'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('gray'))
     
     id = colors.create_rectangle((3.5,70,30,90),fill='brown4')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('brown4'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('brown4'))
     
     id = colors.create_rectangle((3.5,100,30,120),fill='red')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('red'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('red'))
 
     id = colors.create_rectangle((3.5,130,30,150),fill='orange')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('orange'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('orange'))
 
     id = colors.create_rectangle((3.5,160,30,180),fill='yellow')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('yellw'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('yellow'))
 
     id = colors.create_rectangle((3.5,190,30,210),fill='green')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('green'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('green'))
 
     id = colors.create_rectangle((3.5,220,30,240),fill='blue')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('blue'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('blue'))
     
     id = colors.create_rectangle((3.5,250,30,270),fill='purple')
-    colors.tag_bind(id, 'Button-1',lambda x: show_color('purple'))
+    colors.tag_bind(id, '<Button-1>',lambda x: show_color('purple'))
 
 
 
@@ -79,5 +87,23 @@ canvas.place(x=100, y=10)
 canvas.bind('<Button-1>', locate_xy)
 canvas.bind('<B1-Motion>', addLine)
 
+############slider###############
+
+current_value =tk.DoubleVar()
+
+def get_current_value():
+    return '{: .2f}'.format(current_value.get())
+
+def slider_changed(event):
+    value_label.config(text=get_current_value())
+    
+
+slider = ttk.Scale(root,from_=0, to=100, orient='horizontal', command=slider_changed, variable=current_value)
+slider.place(x=30,y=530)
+
+
+#value label
+value_label = ttk.Label(root,text=get_current_value())
+value_label.place(x=27, y=550)
 
 root.mainloop()
